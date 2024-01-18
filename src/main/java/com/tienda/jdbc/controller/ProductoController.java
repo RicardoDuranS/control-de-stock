ba99cdc0862685a26691c1e6d724051d685bb14e
BlastedLattic021 <139406635+RicardoDuranS@users.noreply.github.com>
2024-01-18 16:10:04 -0600

    Se deficio el método listar de ProductoController, el listado ejecuta una query SELECT en la base de datos, el cual devuelve un resultSet. Este resultSet se guarda en una lista de mapas.

--- src/main/java/com/tienda/jdbc/controller/ProductoController.java
+++ src/main/java/com/tienda/jdbc/controller/ProductoController.java
@@ -1,7 +1,16 @@
 package com.tienda.jdbc.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.naming.spi.DirStateFactory.Result;
 
 public class ProductoController {
 
@@ -13,9 +22,32 @@ public class ProductoController {
 		// TODO
 	}
 
-	public List<?> listar() {
-		// TODO
-		return new ArrayList<>();
+	public List<Map<String, String>> listar() throws SQLException {
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost/control_de_stock",
+				"root",
+				"12345678");
+
+		Statement statement = con.createStatement();
+		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+		ResultSet resultSet = statement.getResultSet();
+		List<Map<String, String>> resultado = new ArrayList<>();
+
+		while (resultSet.next()) {
+			Map<String, String> columna = new HashMap<>();
+
+			columna.put("ID", String.valueOf(resultSet.getInt("ID")));
+			columna.put("NOMBRE", resultSet.getString("NOMBRE"));
+			columna.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+			columna.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+
+			resultado.add(columna);
+		}
+
+		con.close();
+
+		return resultado;
 	}
 
 	public void guardar(Object producto) {
