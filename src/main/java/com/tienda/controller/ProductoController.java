c9c72c00dc6076736bd6b7351550ca7148db25c8
BlastedLattic021 <139406635+RicardoDuranS@users.noreply.github.com>
2024-02-14 18:10:54 -0600

    Ejemplo de transacciones incompletas y perdida de stock

--- src/main/java/com/tienda/controller/ProductoController.java
+++ src/main/java/com/tienda/controller/ProductoController.java
@@ -80,6 +80,11 @@ public class ProductoController {
 	}
 
 	public void guardar(Map<String, String> producto) throws SQLException {
+		String nombre = producto.get("NOMBRE");
+		String descripcion = producto.get("DESCRIPCION");
+		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD"));
+		Integer maximoCantidad = 50;
+
 		Connection con = new ConectionFactory().recuperarConexion();
 
 		PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO "
@@ -87,19 +92,30 @@ public class ProductoController {
 				+ " VALUES (?,?,?)",
 				Statement.RETURN_GENERATED_KEYS);
 
-		statement.setString(1, producto.get("NOMBRE"));
-		statement.setString(2, producto.get("DESCRIPCION"));
-		statement.setInt(3, Integer.valueOf(producto.get("CANTIDAD")));
-
-		statement.execute();
+		do {
+			int cantidadParaGuardar = Math.min(cantidad, maximoCantidad);
+			ejecutaRegistro(nombre, descripcion, cantidadParaGuardar, statement);
+			cantidad -= maximoCantidad;
+		} while (cantidad > 0);
 		con.close();
+
+	}
+
+	private void ejecutaRegistro(String nombre, String descripcion, Integer cantidad, PreparedStatement statement)
+			throws SQLException {
+		if (cantidad < 50) {
+			throw new RuntimeException();
+		}
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, cantidad);
+		statement.execute();
 
 		ResultSet resultSet = statement.getGeneratedKeys();
 
 		while (resultSet.next()) {
 			System.out.println(String.format("Fue insertado el producto de ID %d", resultSet.getInt(1)));
 		}
-
 	}
 
 }
